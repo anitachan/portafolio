9990f248475418928f5401013ec14cb85af632e6
anitachan <anita_chavez@outlook.com>
2017-09-14 11:12:09 -0500

    servicio firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,13 +6,29 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargadaSobreNosotros:boolean = false;
+  equipo:any = [];
+
   constructor(public http:Http) {
-    http.get("assets/data/info.pagina.json")
+    this.cargainfo();
+    this.cargaSobreNosotros();
+   }
+
+   public cargainfo(){
+    this.http.get("assets/data/info.pagina.json")
     .subscribe( data => {
-      console.log(data.json());
+      //console.log(data.json());
       this.cargada = true;
       this.info = data.json();
     })
    }
 
+   public cargaSobreNosotros(){
+    this.http.get("https://portafolio-4c8a0.firebaseio.com/equipo.json")
+    .subscribe( data => {
+      console.log(data.json());
+      this.cargadaSobreNosotros = true;
+      this.info = data.json();
+    })
+   }
 }
